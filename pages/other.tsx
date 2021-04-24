ac7d0abc8b600367ae181e8080630cc4889b81cc
bankadmin <chaiyarat.bank@gmail.com>
2021-04-24 15:54:40 +0700

    next-redux-saga

---
+++ pages/other.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { NextPage } from 'next';
+
+import { startClock, tickClock } from '../store/action';
+import Page from '../components/page';
+import { wrapper } from '../store';
+
+const Other: NextPage = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+       dispatch(startClock());
+    });
+
+    return <Page title="Other Page" linkTo="/" NavigateTo="Index Page" />
+};
+
+export const getStaticProps = wrapper.getStaticProps(async ({store}) => {
+    store.dispatch(tickClock(false));
+});
+
+export default Other;
